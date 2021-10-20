f8537bff545676e315f7d0f2a487191ebc2ba8d9
Mahmoud El Omar <31253480+MahmoudElOmar@users.noreply.github.com>
2021-10-20 12:08:37 +0200

    Add files via upload

---
+++ Traffic_Simulator/Paths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Traffic_Simulator
+{
+    public class Paths
+    {
+        public static String iconPath = @"..\Traffic_Simulator\Traffic_Simulator\resources\icon.ico";
+        public static String crossroadCroppedPath = @"..\Traffic_Simulator\Traffic_Simulator\resources\crossroad cropped 2.png";
+        public static String perproadCroppedPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\perproad cropped 2.jpg";
+        public static String mainBackgroundPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\mainWindowBackground.jpg";
+        public static String crossroadIconPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\crossroadIcon.ico";
+        public static String crossroadBackgroundPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\crossroadt.png";
+        public static String spaceRoadIconPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\spaceRoadIcon.ico";
+        public static String spaceRoadBackgroundPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\perproadc.png";
+        public static String spaceshipPhotoPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\top view of spaceships\";
+        public static String carPhotosPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\top view of cars\";
+        public static String redLightPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\redLight.png";
+        public static String yellowLightPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\yellowLight.png";
+        public static String greenLightPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\greenLight.png";
+        public static String redSpaceLightPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\redSpaceTrafficLight.png";
+        public static String greenSpaceLightPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\greenSpaceTrafficLight.png";
+        public static String ohFortunaPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\ohFortuna.wav";
+        public static String radioPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\deathNote.wav";
+        public static String ambulancePhotoPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\Ambulance.png";
+        public static String crossroadPanicBackgroundPath = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\crossroadtPANIC.png";
+        public static String imperialMarch = @"C:\Users\Mahmoud\Desktop\Projects\Traffic_Simulator\Traffic_Simulator\resources\imperialMarch.wav";
+    }
+}
---
+++ Traffic_Simulator/PerpRoad.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Traffic_Simulator
+{
+    public class PerpRoad : Form
+    {
+        SpaceRoad sr1, sr2;
+        Timer spaceWatch;
+        Timer spaceRoadRunner;
+        Timer spaceManager;
+        SoundPlayer imperialMarch;
+        public PerpRoad()
+        {
+            this.Size = new Size(750, 750);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Visible = false;
+            this.Icon = new Icon(Paths.spaceRoadIconPath);
+            this.Text = "Space Road Simulator";
+            this.BackgroundImage = Image.FromFile(Paths.spaceRoadBackgroundPath);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.BackColor = Color.Black;
+
+            this.InitSpaceWatch();
+ 
+
+            sr1 = new SpaceRoad(1, MalemNumerus.mean, MalemNumerus.period);
+            sr2 = new SpaceRoad(2, MalemNumerus.mean, MalemNumerus.period);
+
+            InitSpaceWatch();
+
+            AddSpaceTrafficLight(sr1.stl);
+            AddSpaceTrafficLight(sr2.stl);
+
+            AddAllSpaceships(sr1);
+            AddAllSpaceships(sr2);
+            sr1.stl.ChangeStateTo(true);
+            sr2.stl.ChangeStateTo(false);
+
+            InitSpaceRoadRunner();
+            InitSpaceManager();
+
+
+            this.KeyUp += delegate (object sender, KeyEventArgs e)
+            {
+                if (e.KeyCode == Keys.S)
+                    ShutUp();
+            };
+            
+        }
+        private void InitSpaceManager()
+        {
+            spaceManager = new Timer();
+            spaceManager.Interval = 7000;
+            spaceManager.Enabled = true;
+            spaceManager.Start();
+            spaceManager.Tick += delegate (object sender, EventArgs a)
+            {
+                ManageTrafficLights();
+            };
+        }
+        private void InitSpaceRoadRunner()
+        {
+            spaceRoadRunner = new Timer();
+            spaceRoadRunner.Interval = 1;
+            spaceRoadRunner.Enabled = true;
+            spaceRoadRunner.Start();
+            spaceRoadRunner.Tick += delegate (object sender, EventArgs e)
+            {
+                sr1.DriveSpaceshipsOnSpaceroad();
+                sr2.DriveSpaceshipsOnSpaceroad();
+            };
+        }
+        private void InitSpaceWatch()
+        {
+            spaceWatch = new Timer();
+            spaceWatch.Interval = 1;
+            spaceWatch.Enabled = true;
+            spaceWatch.Start();
+            spaceWatch.Tick += delegate (object sender, EventArgs e)
+            {
+                MalemNumerus.spaceTimeElapsed++;
+            };
+        }
+        private void AddSpaceTrafficLight(SpaceTrafficLight s)
+        {
+            this.Controls.Add(s.redLight);
+            this.Controls.Add(s.greenLight);
+            
+        }
+        private void AddAllSpaceships(SpaceRoad sr)
+        {
+            for (int i = 0; i < sr.NbSpaceships; i++)
+                AddSpaceship(sr.spaceships[i]);
+
+        }
+        private void AddSpaceship(Spaceship c)
+        {
+            this.Controls.Add(c.photo);
+            c.StopMoving();
+        }
+        private async void ManageTrafficLights()
+        {
+            if(sr1.stl.State == true)
+            {
+                sr1.stl.ChangeStateTo(false);
+                await Task.Delay(2000);
+                sr2.stl.ChangeStateTo(true);
+            }
+            else if(sr2.stl.State)
+            {
+                sr2.stl.ChangeStateTo(false);
+                await Task.Delay(2000);
+                sr1.stl.ChangeStateTo(true);
+            }
+
+        }
+        public void InitImperialMarch()
+        {
+            imperialMarch = new SoundPlayer(Paths.imperialMarch);
+            imperialMarch.PlayLooping();
+        }
+        public void ShutUp()
+        {
+            imperialMarch.Stop();
+        }
+
+    }
+}
